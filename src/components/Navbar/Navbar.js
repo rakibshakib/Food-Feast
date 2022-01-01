5191f86b7fdf54d413a44a643915572961693538
aaaasif <alexasif4gmail.com>
2022-01-02 04:49:50 +0600

    Roter V6 Updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { NavLink } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import useAuth from './../../hooks/useAuth';
 
 const Navbar = () => {
+  const { user, logout } = useAuth();
   return (
     <div className="nav-bar">
       <div>
@@ -17,6 +22,19 @@ const Navbar = () => {
           <h2 className="user-name">User name</h2>
           <span> <KeyboardArrowDownIcon /> </span>
         </div>
+        {
+          user?.email ?
+              <Box>
+                  <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/dashboard">
+                      <Button color="inherit">Dashboard</Button>
+                  </NavLink>
+                  <Button onClick={logout} color="inherit">Logout</Button>
+              </Box>
+              :
+              <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/login">
+                  <Button color="inherit">Login</Button>
+              </NavLink>
+          }
       </div>
     </div>
   );
