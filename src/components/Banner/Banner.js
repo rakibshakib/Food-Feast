add6907c5ba75a73b1ef045d51f349a4424f4efd
MunnaAhmed130 <munnaahmed2025@gmail.com>
2022-01-04 01:32:10 +0600

    banner and mid banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,17 +7,14 @@ const Banner = () => {
         <div className='banner'>
             <div className='banner-info'>
                 <h2>Welcome</h2>
-                <Container>
+                <Container className='banner-btn-container'>
                     <Row>
-                        <Col className='p-0'><span className='banner-btn'>Delivery</span></Col>
-                        <Col className='p-0'><span className='banner-btn'>Dine</span></Col>
-                        <Col className='p-0'><span className='banner-btn'>Takeout</span></Col>
+                        <Col className='p-0' sm={6}><span className='banner-btn'>Delivery</span></Col>
+                        <Col className='p-0' sm={6}><span className='banner-btn'>Dine</span></Col>
+                        <Col className='p-0' sm={12}><span className='banner-btn'>Takeout</span></Col>
                     </Row>
                 </Container>
-                <div className='banner-btn-container'>
 
-
-                </div>
                 <p>Order Fast Food Without Destroying Your Diet. We strive to elevate your experience. Eat Best Fat &amp; Furious Burger for Lunch</p>
             </div>
         </div>
