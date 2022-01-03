210c0179172df2f80f801b69a33ae2edaad78fb5
MunnaAhmed130 <munnaahmed2025@gmail.com>
2022-01-04 01:19:46 +0600

    commit

--- src/components/Mid-banner/MidBanner.js
+++ src/components/Mid-banner/MidBanner.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faPlay } from '@fortawesome/free-solid-svg-icons'
 import './MidBanner.css';
+import { faApple, faAppStore, faGooglePlay } from '@fortawesome/free-brands-svg-icons';
+import { Fade } from 'react-reveal';
 
 const MidBanner = () => {
     return (
-        <div className='mid-banner'>
-            <div className='mid-banner-img'>
-                <Row>
-                    <Col>
+        <div className='mid-banner-container'>
+            <div className='mid-banner'>
+                <Row className=' mid-banner-row me-0'>
+                    <Col className='mid-banner-intro' lg={6} sm={12} md={6} >
+                        <div>
+                            <h2>Get your favorite food instantly</h2>
+                            <p className=''>It's all at your fingertips-the taste you love and make the first bite last. Go ahead, Download us</p>
+                            <button variant="contained" className='app-btn' href="https://www.apple.com/app-store/"><FontAwesomeIcon className='icon' icon={faApple} />App Store</button>
+                            <button className='play-btn' href="https://play.google.com/store/apps"><FontAwesomeIcon className='icon' icon={faGooglePlay} />Play Store</button>
+                        </div>
                     </Col>
-                    <Col>
-                        <img src="https://i.ibb.co/kgrWC8x/home-foodpanda-apps.png" alt="" />
+                    <Col lg={6} md={6} sm={12} className='img-col'>
+                        <Fade bottom className=''>
+                            <img className='mid-banner-img' src="https://i.ibb.co/kgrWC8x/home-foodpanda-apps.png" alt="" />
+                        </Fade>
                     </Col>
                 </Row>
-
             </div>
         </div>
     );
 };
+
+// #ff0022,         Secondery
+// #edfdfb          Primary
 
 export default MidBanner;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Footer from '../../components/Footer/Footer'
 import HomePageProducts from '../../components/HomePageProducts/HomePageProducts'
-import Banner from '../../components/Navbar/Banner'
+import MidBanner from '../../components/Mid-banner/MidBanner'
+import Banner from '../../components/Banner/Banner'
 import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
 const Home = () => {
     return (
         <div>
-            <Navbar />
             <Banner />
             <HomePageProducts />
-            <Footer />
+            <MidBanner />
+            {/* <Footer /> */}
         </div>
     )
 }
