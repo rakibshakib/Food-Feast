4606827f815af911ff7594e6e3de4671a5f4d78d
rakibshakib <rakibshakib74@gmail.com>
2022-01-04 16:40:23 +0600

    updated home page wiht faq

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,18 +3,7 @@ import Footer from '../../components/Footer/Footer'
 import HomePageProducts from '../../components/HomePageProducts/HomePageProducts'
 import MidBanner from '../../components/Mid-banner/MidBanner'
 import Banner from '../../components/Banner/Banner'
-<<<<<<< HEAD
-
-
-import Navbar from '../../components/Navbar/Navbar'
-
 import FAQ from './FAQ'
-
-
-import Banner from '../../components/Navbar/Banner'
-
-=======
->>>>>>> rakibul-islam
 import Header from '../../components/Navbar/Header'
 
 
@@ -22,29 +11,13 @@ import './Home.css'
 const Home = () => {
     return (
         <div>
-<<<<<<< HEAD
-=======
-
-
->>>>>>> rakibul-islam
             <Header />
-
             <Banner />
             <HomePageProducts />
-
             <FAQ />
             <Footer />
-
             <MidBanner />
-<<<<<<< HEAD
-
             <Footer />
-
-            {/* <Footer /> */}
-
-=======
-            <Footer />
->>>>>>> rakibul-islam
         </div>
     )
 }
