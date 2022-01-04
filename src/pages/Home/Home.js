5bbad9ba6c005e8211451241232080b58be8587a
aaaasif <alexasif4gmail.com>
2022-01-04 18:30:35 +0600

    FAQ added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,12 +3,8 @@ import Footer from '../../components/Footer/Footer'
 import HomePageProducts from '../../components/HomePageProducts/HomePageProducts'
 import MidBanner from '../../components/Mid-banner/MidBanner'
 import Banner from '../../components/Banner/Banner'
-import Navbar from '../../components/Navbar/Navbar'
 
 import FAQ from './FAQ'
-
-
-import Banner from '../../components/Navbar/Banner'
 import Header from '../../components/Navbar/Header'
 
 
