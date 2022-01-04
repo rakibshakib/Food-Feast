64daf66c4c5c311a71efdbe040a554c1c40e278d
MunnaAhmed130 <munnaahmed2025@gmail.com>
2022-01-04 13:48:29 +0600

    banner and mid banner

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
             <Banner />
             <HomePageProducts />
             <MidBanner />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
