4d633d41ed495554a425df2979115b26b53a7d01
rakibshakib <rakibshakib74@gmail.com>
2022-01-05 20:04:03 +0600

    update hooks

--- src/Hooks/useFoodData.js
+++ src/Hooks/useFoodData.js
@@ -1,12 +1,25 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+const axios = require('axios');
+
 
 export const useFoodData = () => {
   const [foods, setFoods] = useState([]);
-  useEffect(() => {
-    fetch(`https://pacific-refuge-96429.herokuapp.com/foods`)
-      .then((res) => res.json())
-      .then((data) => setFoods(data));
-  }, []);
+  axios.get('https://pacific-refuge-96429.herokuapp.com/foods')
+  .then(function (response) {
+    setFoods(response.data)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+  // useEffect(() => {
+  //   fetch(`https://pacific-refuge-96429.herokuapp.com/foods`)
+  //     .then((res) => res.json())
+  //     .then((data) => setFoods(data));
+  // }, []);
 
   return [foods];
 };
